31590fe39615a2a6742131f8f848353194a60892
Melanie Schwartz <101142745+melschwartz07@users.noreply.github.com>
2022-05-18 16:58:51 -0500

    methods exercise in work

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -12,6 +12,8 @@ public class MethodsExercises {
         System.out.println("divide two numbers: " + obj.divide(50, 10));
         //call getInteger method
         getInteger(1,10);
+        //call factorial method
+        factorial(1,10);
     }
 
 // TODO: Basic Arithmetic
@@ -44,17 +46,41 @@ public class MethodsExercises {
 // TODO: Create a method that validates that user input is in a certain range
 //    If the input is invalid, prompt the user again.
 
-    public static void getInteger(int min, int max) {
-        System.out.println("Enter a number between 1 and 10: ");
+    public static int getInteger(int min, int max) {
+        System.out.print("Enter a number between 1 and 10: ");
         Scanner scanner = new Scanner(System.in);
 
         int userInput = scanner.nextInt();
 
-        if (userInput < min || userInput > max){
-            System.out.print("The number you entered is not valid");
-        } else {
-            System.out.print(userInput + " is in range!");
+        while (userInput < min || userInput > max) {
+            System.out.print("The number you entered is not valid. Please try again: ");
+            userInput = scanner.nextInt();
         }
+        while (userInput <= 10) {
+            System.out.println(userInput + " is a valid number!");
+            break;
+        }
+        return 1;
     }
+
+// TODO: Calculate the factorial of a number.
+//  Prompt the user to enter an integer from 1 to 10.
+//    Display the factorial of the number entered by the user.
+//    Ask if the user wants to continue.
+//    Use a for loop to calculate the factorial.
+//    Assume that the user will enter an integer, but verify it’s between 1 and 10.
+//    Use the long type to store the factorial.
+//    Continue only if the user agrees to.
+
+    public static int factorial(int min, int max){
+        System.out.print("User, please enter a number from 1 to 10: ");
+        Scanner scanner = new Scanner(System.in);
+        int userInput = scanner.nextInt();
+
+
+
+
+    }
+
 
 }
