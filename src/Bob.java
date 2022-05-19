881622d882436b3483691b11026eb5a13c9a0ded
Melanie Schwartz <101142745+melschwartz07@users.noreply.github.com>
2022-05-19 15:19:04 -0500

    bob complete

--- src/Bob.java
+++ src/Bob.java
@@ -1,5 +1,6 @@
+import java.util.Scanner;
 public class Bob {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
 // TODO: Bob is a lackadaisical teenager. In conversation, his responses are very limited.
 //  Bob answers 'Sure.' if you ask him a question. (the input ends with a question mark)
@@ -7,6 +8,31 @@ public class Bob {
 //        He says 'Fine. Be that way!' if you address him without actually saying anything. (empty input)
 //        He answers 'Whatever.' to anything else.
 //        Write the Java code necessary so that a user of your command line application can have a conversation with Bob.
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hi, I'm Bob.");
+        String userInput;
+
+        do {
+            userInput = scanner.nextLine();
+            if (userInput.endsWith("?")) {
+                System.out.println("sure.");
+            } else if (userInput.endsWith("!")) {
+                System.out.println("whoa, chill out.");
+            } else if (userInput.equals("")) {
+                System.out.println("fine, be that way.");
+            } else {
+                System.out.println("whatever");
+            }
+            System.out.println("userInput before conditional: " + userInput);
+        } while (userInput == "bye");
+        {
+
+            System.out.println("finally. bye.");
+
+
+        }
+
 
     }
 }
