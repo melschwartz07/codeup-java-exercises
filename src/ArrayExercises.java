b7a83d30ae36829f7a34bad167487466fce5b091
Melanie Schwartz <101142745+melschwartz07@users.noreply.github.com>
2022-05-26 16:57:27 -0500

    array exercises initial commit

--- src/ArrayExercises.java
+++ src/ArrayExercises.java
@@ -13,6 +13,7 @@ public class ArrayExercises {
 
 // TODO: Create an array that holds 3 Person objects. Assign a new instance of the Person class to each element.
 //  Iterate through the array and print out the name of each person in the array.
+
 //  Create a static method named addPerson. It should accept an array of Person objects, as well as a single person
 //  object to add to the passed array. It should return an array whose length is 1 greater than the passed array,
 //  with the passed person object at the end of the array.
@@ -26,5 +27,7 @@ public class ArrayExercises {
         for (Person someone : names) {
             System.out.println(someone.getName());
         }
+
+        
     }
 }
